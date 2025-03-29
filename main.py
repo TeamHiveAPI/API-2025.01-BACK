e9c8aad98d3ed85f0a3da01d92ee300ec5544f2e
Juan Garcia Soares <juan.garcia.soares@gmail.com>
2025-03-29 17:49:41 -0300

    [SCRUM-3] feat: implementado CRUD de parametros e tipo parametros, incluindo schemas dos mesmos

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import create_tables
 from contextlib import asynccontextmanager
-import models  # Importa os modelos para registrá-los na Base
+import models
+from routes import parametro, tipo_parametro
 
 # Definindo o manipulador de ciclo de vida
 @asynccontextmanager
@@ -16,6 +18,22 @@ app = FastAPI(
     version="0.1.0",
     lifespan=lifespan
 )
+
+# Configuração do CORS
+origins = [
+    "http://localhost:5173",  # Frontend do React
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,         # Permite requisições do domínio listado
+    allow_credentials=True,        # Permite envio de cookies, se necessário
+    allow_methods=["*"],           # Permite todos os métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],           # Permite todos os cabeçalhos
+)
+
+app.include_router(parametro.router)
+app.include_router(tipo_parametro.router)
 
 # Endpoint simples para testar
 @app.get("/")
