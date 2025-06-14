35ff34c7ce8bd095fbe966f4c788e13350dc8c52
Erika Dias Ribeiro <diasribeiroerica4@gmail.com>
2025-06-14 12:18:24 -0300

    atualizei o localhost do postgre

--- database.py
+++ database.py
@@ -1,27 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# URL de conexão com o PostgreSQL
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/api"
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("Variável DATABASE_URL não definida em ENV")
 
+engine = create_engine(DATABASE_URL, echo=True, pool_pre_ping=True)
 
-# Criar a engine de conexão
-engine = create_engine(DATABASE_URL, echo=True)
-
-# Configurar a sessão
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base para os modelos
 Base = declarative_base()
 
-# Função para criar as tabelas
 def create_tables():
-   
-
     Base.metadata.create_all(bind=engine)
 
-# Função para gerenciar a sessão do banco
 def get_db():
     db = SessionLocal()
     try:
